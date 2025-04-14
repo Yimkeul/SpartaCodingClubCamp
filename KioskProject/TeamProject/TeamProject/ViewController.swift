2a30b118317e9559da8a676eabe024dddd93f3c0
Yimkeul <leesjun29@gmail.com>
2025-04-14 15:23:46 +0900

    upload project

---
+++ KioskProject/TeamProject/TeamProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TeamProject
+//
+//  Created by 서동환 on 4/4/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
