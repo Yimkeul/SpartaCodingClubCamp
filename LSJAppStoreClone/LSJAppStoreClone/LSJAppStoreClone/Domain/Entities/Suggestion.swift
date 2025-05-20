d1b3e943dafd5236cbd91c3e01926928b6884f1f
Yimkeul <leesjun29@gmail.com>
2025-05-20 14:13:20 +0900

    upload

---
+++ LSJAppStoreClone/LSJAppStoreClone/LSJAppStoreClone/Domain/Entities/Suggestion.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestionType.swift
+//  LSJAppStoreClone
+//
+//  Created by yimkeul on 5/13/25.
+//
+
+import Foundation
+
+enum Suggestion {
+    case movie(movie: Movie, index: Int)
+    case podcast(podcast: Podcast, index: Int)
+}
